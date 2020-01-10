caf0a1fb7b4a43cb9d422f95af604f056a3f9f55
rzfvansuijdam <55277122+rzfvansuijdam@users.noreply.github.com>
2020-01-10 14:33:50 +0100

    les 10 january

--- 01_huisje/script.js
+++ 01_huisje/script.js
@@ -4,10 +4,48 @@ const canvas = document.getElementById('canvas');
 // leg een 2d-context over de canvas
 const context = canvas.getContext('2d');
 
-//maak de canvas schermvullend
+// lolz
 const width = window.innerWidth;
 const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-// hier kun je op de canvas tekenen
+
+context.beginPath()
+context.fillStyle = "grey";
+context.moveTo(700,200);
+context.lineTo(600,400);
+context.lineTo(800,300);
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "red";
+context.moveTo(700,200);
+context.lineTo(600,400);
+context.lineTo(200,300);
+context.lineTo(300,100);
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "#A0A0A0";
+context.moveTo(200,300);
+context.lineTo(600,400);
+context.lineTo(600,600);
+context.lineTo(200,500);
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "grey";
+context.moveTo(600,400);
+context.lineTo(600,600);
+context.lineTo(800,500);
+context.lineTo(800,300);
+context.closePath();
+context.stroke();
+context.fill()
